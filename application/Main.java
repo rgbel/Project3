f6be34c1493c693b1ff0cfa6d864d5793c2f4d6c
mattpess <37192339+mattpess@users.noreply.github.com>
2018-03-24 13:50:29 -0400

    Add files via upload

--- application/Main.java
+++ application/Main.java
@@ -4,36 +4,22 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
-import application.WarehouseFleet;
 
 
 public class Main extends Application {
-	
-	// This variable should be used instead of directly indicating the file's name.
-	//This allows for the databaseFile to be internally modified without having to search for all instances of it.
-	public static final String databaseFile = "warehouseDB.txt";
-	public static WarehouseFleet programFleet = new WarehouseFleet(WarehouseFleet.fileToFleet(databaseFile));
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			primaryStage.setTitle("Bike Part Warehouse Manager");
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("main.fxml"));
+			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-
-			        public void run() {
-			            programFleet.fleetToFile(databaseFile);
-			        }
-			    }));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
