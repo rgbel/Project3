3b52727415a23885ab02043e4d6c084717407943
mattpess <37192339+mattpess@users.noreply.github.com>
2018-03-24 13:43:02 -0400

    Add files via upload

---
+++ PartInterface.java
@@ -0,0 +1,12 @@
+package application;
+
+public interface PartInterface {
+
+	String getName();
+	
+	int getNumber();
+	
+	void setName(String newName);
+	
+	void setNumber(int newNumber);
+}
---
+++ SystemAdmin.java
@@ -0,0 +1,12 @@
+package roster;
+
+public class SystemAdmin extends Employee{
+
+	public SystemAdmin(String nameFirst, String nameLast, String phoneNumber, String email, LoginAccount loginInfo) {
+		super(nameFirst, nameLast, phoneNumber, email, loginInfo);
+		this.getLoginInfo().changePermission(0);
+	}
+	
+	public SystemAdmin() {}
+
+}
