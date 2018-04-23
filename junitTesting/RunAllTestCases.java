ea44bbc93f443f469be871c9cb8a65bae01b076c
rgbel <37554908+rgbel@users.noreply.github.com>
2018-04-23 18:46:43 -0400

    Add files via upload

--- junitTesting/RunAllTestCases.java
+++ junitTesting/RunAllTestCases.java
@@ -5,9 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 	@RunWith(Suite.class)
-	@SuiteClasses({SalesInvoiceTest.class})
-public class RunAllTestCases {
-	
+
+	@SuiteClasses({SalesInvoiceTest.class,LoginAccountTest.class,EmployeeTest.class})
+
+public class RunAllTestCases {	
+
 		//Running this class will run all TestCases, as long as they are in the SuiteClasses definition
+
 		
+
 }
